0bd85cb14a1731340470c4f2bf2eaa43b6bab107
krav-dev <84710621+krav-dev@users.noreply.github.com>
2021-05-24 14:44:39 +0500

    Add files via upload

---
+++ power-builders/js/main.js
@@ -0,0 +1,22 @@
+$(function(){
+$('.slider-header').slick({
+	dots:true,
+	initialSlide:1,
+});
+
+$('.our-slider').slick({
+ dots:true,
+ arrows:false,
+ initialSlide:1,
+});
+$('.testimon-slider').slick({
+
+});
+$('.partner__slider').slick({
+	arrows:false,
+	dots:true,
+	initialSlide:1,
+
+});
+
+});
